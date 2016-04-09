01ed8ee224b262bc3d5f4192e1b5cdf282adc45e
Clarence Davis Jr <cldavisjr@gmail.com>
2016-04-09 06:54:06 -0500

    bumped to latest

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-maploom',
-    version='0.0.1@2016-04-07.12:05:05.7fd2309c66',
+    version='0.0.1@2016-04-09.06:52:46.ae876a869f',
     author='LMN Solutions',
     author_email='geoshape.org@gmail.com',
     url='https://github.com/ROGUE-JCTD/django-maploom',
