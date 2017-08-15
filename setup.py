986bbf2caed95c11656237c6de656d773ec7b1fe
davisc <cdavis@boundlessgeo.com>
2017-08-15 23:12:25 +0000

    jenkins job django-maploom: use latest maploom to build maploom django wrapper.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-maploom',
-    version='0.0.1@2017-04-26.16:20:36.a189ba457b',
+    version='0.0.1@2017-08-15.23:12:25.5964b4a353',
     author='LMN Solutions',
     author_email='geoshape.org@gmail.com',
     url='https://github.com/ROGUE-JCTD/django-maploom',
